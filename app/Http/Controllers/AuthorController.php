ac6249428aeb8090cbd659a2f2cc8c0b944c3362
irma-developer <sitiirmawatiirma39@gmail.com>
2025-10-17 14:35:08 +0700

    Read all data,Create data genre dan author

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,17 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Author;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
+    // GET /api/authors
     public function index()
     {
-        $authors = Author::withCount('books')->orderBy('name')->get();
+        // kalau mau ikut jumlah buku: Author::withCount('books')->orderBy('name')->get();
+        $authors = Author::orderBy('name')->get();
 
         return response()->json([
-            "success" => true,
-            "message" => "Get all authors",
-            "data" => $authors
+            'success' => true,
+            'message' => 'Get all authors',
+            'data'    => $authors,
         ], 200);
+    }
+
+    // POST /api/authors
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:150|unique:authors,name',
+            'bio'  => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation errors',
+                'data'    => $validator->errors(),
+            ], 422);
+        }
+
+        $author = Author::create($validator->validated());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Author created',
+            'data'    => $author,
+        ], 201);
     }
 }
